378a8de9ecaab75ba679edae0ddf5386304916d1
RaeSimon <152085766+RaeSimon@users.noreply.github.com>
2025-07-26 11:13:22 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bed",
+      "name": "Bed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.02180733189998918,
+        "pitch": 0,
+        "fov": 1.3591912368623837
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.006184439800451358,
+          "pitch": 0.20678598587319996,
+          "rotation": 0,
+          "target": "0-bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-walk-in-closet-2",
+      "name": "Walk-in-closet 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3591912368623837
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.37282049418369567,
+          "pitch": 1.2847352011180853,
+          "rotation": 0,
+          "target": "1-walk-in-closet-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
